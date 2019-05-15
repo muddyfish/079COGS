248ac0f8267152cd3275cbdf25f9b73b4d83d480
Blue <redacted>
2019-05-15 01:35:07 +0100

    Fix a few self issues

--- leveler/on_message.py
+++ leveler/on_message.py
@@ -11,7 +11,7 @@ from .static_methods import _add_corners, _required_exp, _center, _contrast
 prefix = "!"
 
 
-async def draw_levelup(self, user, guild):
+async def draw_levelup(leveler, user, guild):
     # fonts
     font_thin_file = "fonts/Uni_Sans_Thin.ttf"
     level_fnt = ImageFont.truetype(font_thin_file, 23)
@@ -22,15 +22,15 @@ async def draw_levelup(self, user, guild):
     bg_url = userinfo["levelup_background"]
     profile_url = user.avatar_url
 
-    async with self.session.get(bg_url) as r:
+    async with leveler.session.get(bg_url) as r:
         image = await r.content.read()
     with open("temp/{}_temp_level_bg.png".format(str(user.id)), "wb") as f:
         f.write(image)
     try:
-        async with self.session.get(profile_url) as r:
+        async with leveler.session.get(profile_url) as r:
             image = await r.content.read()
     except:
-        async with self.session.get(default_avatar_url) as r:
+        async with leveler.session.get(default_avatar_url) as r:
             image = await r.content.read()
     with open("temp/{}_temp_level_profile.png".format(str(user.id)), "wb") as f:
         f.write(image)
@@ -123,17 +123,17 @@ async def draw_levelup(self, user, guild):
     result.save(filename, "PNG", quality=100)
 
 
-async def _handle_on_message(self, message):
+async def _handle_on_message(leveler, message):
     # try:
     text = message.content
     guild = message.guild
     user = message.author
     # creates user if doesn't exist, bots are not logged.
-    await self._create_user(user, guild)
+    await leveler._create_user(user, guild)
     curr_time = time.time()
     userinfo = db.users.find_one({"user_id": str(str(user.id))})
 
-    if not guild or str(guild.id) in self.settings["disabled_guilds"]:
+    if not guild or str(guild.id) in leveler.settings["disabled_guilds"]:
         return
     if user.bot:
         return
@@ -145,14 +145,14 @@ async def _handle_on_message(self, message):
     if float(curr_time) - float(userinfo["chat_block"]) >= 120 and not any(
             text.startswith(x) for x in prefix
     ):
-        await _process_exp(self, message, userinfo, random.randint(15, 20))
-        await _give_chat_credit(self, user, guild)
+        await _process_exp(leveler, message, userinfo, random.randint(15, 20))
+        await _give_chat_credit(leveler, user, guild)
 
 
-async def _give_chat_credit(self, user, guild):
+async def _give_chat_credit(leveler, user, guild):
     try:
-        if "msg_credits" in self.settings:
-            await bank.deposit_credits(user, self.settings["msg_credits"][str(guild.id)])
+        if "msg_credits" in leveler.settings:
+            await bank.deposit_credits(user, leveler.settings["msg_credits"][str(guild.id)])
     except:
         pass
 
@@ -206,31 +206,31 @@ async def _process_exp(leveler, message, userinfo, exp: int):
 
 
 async def _handle_levelup(leveler, user, userinfo, guild, channel):
-    if not isinstance(self.settings["lvl_msg"], list):
-        self.settings["lvl_msg"] = []
-        fileIO("settings.json", "save", self.settings)
+    if not isinstance(leveler.settings["lvl_msg"], list):
+        leveler.settings["lvl_msg"] = []
+        fileIO("settings.json", "save", leveler.settings)
     guild_identifier = ""  # super hacky
-    name = self._is_mention(user)  # also super hacky
+    name = leveler._is_mention(user)  # also super hacky
     new_level = str(userinfo["servers"][str(guild.id)]["level"])
-    if str(guild.id) in self.settings["lvl_msg"]:  # if lvl msg is enabled
+    if str(guild.id) in leveler.settings["lvl_msg"]:  # if lvl msg is enabled
         # channel lock implementation
         if (
-                "lvl_msg_lock" in self.settings.keys()
-                and str(guild.id) in self.settings["lvl_msg_lock"].keys()
+                "lvl_msg_lock" in leveler.settings.keys()
+                and str(guild.id) in leveler.settings["lvl_msg_lock"].keys()
         ):
-            channel_id = self.settings["lvl_msg_lock"][str(guild.id)]
+            channel_id = leveler.settings["lvl_msg_lock"][str(guild.id)]
             channel = find(lambda m: m.id == channel_id, guild.channels)
 
         # private message takes precedent, of course
         if (
-                "private_lvl_msg" in self.settings
-                and str(guild.id) in self.settings["private_lvl_msg"]
+                "private_lvl_msg" in leveler.settings
+                and str(guild.id) in leveler.settings["private_lvl_msg"]
         ):
             guild_identifier = " on {}".format(guild.name)
             channel = user
             name = "You"
 
-        if "text_only" in self.settings and str(guild.id) in self.settings["text_only"]:
+        if "text_only" in leveler.settings and str(guild.id) in leveler.settings["text_only"]:
 
             em = discord.Embed(
                 description="**{} just gained a level{}! (LEVEL {})**".format(
@@ -240,7 +240,7 @@ async def _handle_levelup(leveler, user, userinfo, guild, channel):
             )
             await channel.send("", embed=em)
         else:
-            await self.draw_levelup(user, guild)
+            await leveler.draw_levelup(user, guild)
 
             await channel.send(
                 "**{} just gained a level{}!**".format(name, guild_identifier),
