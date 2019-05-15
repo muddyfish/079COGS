37e71ff9ae35c25bada18fea3bb5b236075d1cef
Blue <redacted>
2019-05-15 14:47:25 +0100

    Don't error in DMs

--- leveler/permissions.py
+++ leveler/permissions.py
@@ -5,6 +5,8 @@ from .config import db
 @commands.check
 async def leveler_enabled(ctx: commands.Context):
     guild = ctx.message.guild
+    if guild is None:
+        return True
     settings = await db.settings()
     if str(guild.id) in settings["disabled_guilds"]:
         await ctx.send("**Leveler commands for this guild are disabled!**")
